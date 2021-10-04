94f6f4d79ee0b6c0889617e4c5fb39e3e750cab2
Yash Hirani <86243150+yashdothirani@users.noreply.github.com>
2021-10-04 21:57:06 +0530

    Initial Commit

---
+++ JDBC/app14/src/com/yash/jdbc/JdbcApp14.java
@@ -0,0 +1,46 @@
+package com.yash.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JdbcApp14 {
+
+	public static void main(String[] args) {
+		Connection con = null;
+		Statement st = null;
+		ResultSet rs = null;
+
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "yash");
+			st = con.createStatement();
+			boolean b = st.execute("select * from emp1");
+			rs = st.getResultSet();
+
+			while (rs.next()) {
+				System.out.print("\t" + rs.getInt(1));
+				System.out.print("\t" + rs.getString(2));
+				System.out.print("\t" + rs.getFloat(3));
+				System.out.println("\t" + rs.getString(4));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+		} finally {
+
+			try {
+				rs.close();
+				st.close();
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
