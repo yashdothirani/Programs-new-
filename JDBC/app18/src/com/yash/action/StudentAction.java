f1e53c26fbf2c932c20b093caf6f52ebcfb19c30
Yash Hirani <86243150+yashdothirani@users.noreply.github.com>
2021-10-06 16:40:22 +0530

    Initial Commit

--- JDBC/app18/src/com/yash/action/StudentAction.java
+++ JDBC/app18/src/com/yash/action/StudentAction.java
@@ -9,9 +9,10 @@ import java.sql.Statement;
 import com.yash.beans.Student;
 
 public class StudentAction {
-Connection con = null;
-Statement st = null;
-Student student ;
+Connection con;
+Statement st;
+ResultSet rs;
+Student std ;
 String status = "";
 
 	public StudentAction()throws Exception {
@@ -21,28 +22,48 @@ String status = "";
 		
 	}
 	
-	public String addStudent(String sid,String sname , String saddr) {
+	public String addStudent(String sid,String sname , String saddr) throws Exception {
+		int rowCount = st.executeUpdate("insert into student values('"+sid+"','"+sname+"','"+saddr+"')");
+		if(rowCount == 1)
+			status = "existed";
+		else
+			status = "not existed";
 		return status;
 	}
 	
 	public Student searchStudent(String sid) throws Exception {
-		ResultSet rs = st.executeQuery("select * from student");
-		while(rs.next()) {
-			if(rs.getString(1).equals(sid)) {
-				student.setSid(rs.getString(1));
-				student.setSname(rs.getString(2));
-				student.setSaddr(rs.getString(3));
-				return student;
-			}
+		rs = st.executeQuery("select * from student where sid = '"+ sid + "'");
+		boolean b = rs.next();
+		if(b == true) {
+			std = new Student();
+			
+				std.setSid(rs.getString(1));
+				std.setSname(rs.getString(2));
+				std.setSaddr(rs.getString(3));
 		}
-		return null;
-	}
+		else {
+			std = null;
+		}
+		return std;
+		}
 	
-	public String updateStudent(String sid,String sname,String saddr) {
+	public String updateStudent(String sid,String sname,String saddr) throws Exception {
+		int rowCount = st.executeUpdate("update student set sname = '"+sname+"',saddr = '"+saddr+"' where sid = '"+sid+"'");
+		if(rowCount == 1)
+			status = "updated";
+		else
+			status = "not updated";
+		
+		
 		return status;
 	}
 	
-	public String deleteStudent(String sid) {
+	public String deleteStudent(String sid) throws Exception {
+		int rowCount = st.executeUpdate("delete student where sid = '" +sid+"'");
+		if(rowCount == 1)
+			status = "deleted";
+		else
+			status = "not deleted";
 		return status;
 	}
 	
--- JDBC/app18/src/com/yash/jdbc/JdbcApp18.java
+++ JDBC/app18/src/com/yash/jdbc/JdbcApp18.java
@@ -1,6 +1,5 @@
 package com.yash.jdbc;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,42 +11,108 @@ public class JdbcApp18 {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StudentAction std = new StudentAction();
-		
+		StudentAction stdAction = new StudentAction();
+		Student std;
+
 		while (true) {
+			System.out.println("=============================");
 			System.out.println("1.add student\n2.search student\n3.update student\n4.delete student\n5.exit");
 			System.out.println("=============================");
 			System.out.print("your choice : ");
 			String choice = br.readLine();
-			String sid = "S-";String sname = "";String saddr = "";String status = "";
-			switch(choice) {
+			String sid = "S-";
+			String sname = "";
+			String saddr = "";
+			String status = "";
+			switch (choice) {
 			case "1":
-				
+				System.out.println("-----------------------------");
+				System.out.print(" enter sid : ");
+				sid += br.readLine();
+				std = stdAction.searchStudent(sid);
+				if (std == null) {
+					System.out.print("enter sname : ");
+					sname = br.readLine();
+					System.out.print("enter saddr : ");
+					saddr = br.readLine();
+					status = stdAction.addStudent(sid, sname, saddr);
+					if (status.equals("existed"))
+						System.out.println("student added successfully.");
+					else
+						System.out.println("student addition failure.");
+
+				} else {
+					System.out.println("student already existed");
+				}
+				System.out.println("-----------------------------");
 				break;
 			case "2":
-				Student student = std.searchStudent(sid);
+				System.out.println("-----------------------------");
 				System.out.print("Enter student id to search : ");
 				sid += br.readLine();
-				
-				
-				if (status.equals("existed")) {
-					System.out.println("sid : " + student.getSid());
-					System.out.println("sname : " + student.getSname());
-					System.out.println("saddr : " + student.getSaddr());
-				}else {
+				std = stdAction.searchStudent(sid);
+				;
+				if (std != null) {
+
+					System.out.println("sid : " + std.getSid());
+					System.out.println("sname : " + std.getSname());
+					System.out.println("saddr : " + std.getSaddr());
+
+				} else {
 					System.out.println("student not existed");
 				}
-				
+				System.out.println("-----------------------------");
 				break;
 			case "3":
+				System.out.println("-----------------------------");
+				System.out.print("Enter student id to update : ");
+				sid += br.readLine();
+				std = stdAction.searchStudent(sid);
+				if (std != null) {
+					System.out.print("sname : OLD: "+std.getSname()+" | NEW: ");
+					sname = br.readLine();
+					if (sname.equals(""))
+						sname = std.getSname();
+					System.out.print("saddr : OLD: "+std.getSaddr()+" | NEW: ");
+					saddr = br.readLine();
+					if (saddr.equals(""))
+						saddr = std.getSaddr();
+					
+					status = stdAction.updateStudent(sid, sname, saddr);
+					if (status.equals("updated"))
+						System.out.println("student details updated successfully.");
+					else
+						System.out.println("updation failure");
+					
+				} else {
+					System.out.println("student not existed");
+				}
+				System.out.println("-----------------------------");
 				break;
 			case "4":
+				System.out.println("-----------------------------");
+				System.out.print("Enter student id to delete : ");
+				sid += br.readLine();
+				std = stdAction.searchStudent(sid);
+				if (std != null) {
+					status = stdAction.deleteStudent(sid);
+					if(status.equals("deleted")) {
+						System.out.println("Student deleted successfully.");
+			
+					}
+					else
+						System.out.println("deletion failure.");
+				}else 
+					System.out.println("student not existed");
+				
+				
+				System.out.println("-----------------------------");
 				break;
 			case "5":
 				System.out.println("Thank you for using the application");
 				System.exit(0);
 			}
-			
+
 		}
 
 	}
