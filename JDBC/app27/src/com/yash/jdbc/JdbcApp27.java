814db44f1daf341763e9843f7f7bb0dd83a0306b
Yash Hirani <86243150+yashdothirani@users.noreply.github.com>
2021-10-07 14:31:54 +0530

    Initial Commit

---
+++ JDBC/app27/src/com/yash/jdbc/JdbcApp27.java
@@ -0,0 +1,42 @@
+package com.yash.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class JdbcApp27 {
+
+	public static void main(String[] args) {
+		try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/yashdb", "root", "yash");
+				Statement st = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);//Not supported by type 4 driver by oracle
+				ResultSet rs = st.executeQuery("select * from emp1");
+
+		) {
+			while(rs.next()) {
+			System.out.print(rs.getInt(1) + "\t");
+			System.out.print(rs.getString(2) + "\t");
+			System.out.print(rs.getFloat(3) + "\t");
+			System.out.println(rs.getString(4) + "\t");
+			}
+			
+			System.in.read();
+			
+			System.out.println("---------------------------------------");
+			rs.beforeFirst();
+			while(rs.next()) {
+				rs.refreshRow();
+				System.out.print(rs.getInt(1) + "\t");
+				System.out.print(rs.getString(2) + "\t");
+				System.out.print(rs.getFloat(3) + "\t");
+				System.out.println(rs.getString(4) + "\t");
+				}
+			
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
