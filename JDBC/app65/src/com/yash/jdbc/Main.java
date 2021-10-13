3ca4e55159ee5f26e47f184233e91de999ffddab
Yash Hirani <86243150+yashdothirani@users.noreply.github.com>
2021-10-13 15:41:20 +0530

    Initial Commit

--- JDBC/app65/src/com/yash/jdbc/Main.java
+++ JDBC/app65/src/com/yash/jdbc/Main.java
@@ -1,26 +1,27 @@
 package com.yash.jdbc;
 
 import org.logicalcobwebs.proxool.ProxoolDataSource;
+import org.logicalcobwebs.proxool.ProxoolDriver;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        try {
+	// write your code here
+Connection con = null;
+try {
 
             ProxoolDataSource ds = new ProxoolDataSource();
-            ds.setDriver("oracle.jdbc.OracleDriver");
-            ds.setDriverUrl("jdbc:oracle:thin:@localhost:1521:xe");
-            ds.setUser("system");
+            ds.setDriver("com.mysql.cj.jdbc.Driver");
+            ds.setDriverUrl("jdbc:mysql://localhost:3306/yashdb");
+            ds.setUser("root");
             ds.setPassword("yash");
             ds.setMinimumConnectionCount(5);
             ds.setMaximumConnectionCount(10);
-            Connection con = ds.getConnection();
+            con = ds.getConnection();
             Statement st = con.createStatement();
             ResultSet rs = st.executeQuery("select * from emp1");
             while (rs.next()) {
