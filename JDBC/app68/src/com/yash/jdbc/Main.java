cedd22a92948d224d976aa2d72846cf2996fe9db
Yash Hirani <86243150+yashdothirani@users.noreply.github.com>
2021-10-14 21:47:29 +0530

    Initial Commit

--- JDBC/app68/src/com/yash/jdbc/Main.java
+++ JDBC/app68/src/com/yash/jdbc/Main.java
@@ -31,7 +31,7 @@ public class Main {
                     rowSet.updateRow();
                 }
             }
-
+rowSet.close();
         }catch (Exception e){
             e.printStackTrace();
         }
